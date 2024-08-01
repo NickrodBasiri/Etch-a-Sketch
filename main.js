6c6a1b33758d8484efdcba79e9aa9e7e8813d74a
NickrodBasiri <basirinickrod@gmail.com>
2024-07-31 22:25:05 -0400

    Added Change Grid button and sketching.

--- main.js
+++ main.js
@@ -1,12 +1,47 @@
-gridSize = 30;
+let gridSize = 16;
 const container = document.querySelector("#container");
-
-const sideLength = `${700/gridSize}`;
 
 for (i = 1; i <= (gridSize ** 2); i++) {
     const square = document.createElement("div");
+    const sideLength = `${700/gridSize}`;
     square.style.cssText = `height: ${sideLength}px`;
     square.style.cssText = `width: ${sideLength}px`;
     square.id = "square";
+    square.addEventListener("mousemove", function() {
+        square.style.backgroundColor = "black";
+    })
     container.appendChild(square);
 }
+
+const gridButton = document.querySelector("#changeGrid");
+gridButton.addEventListener("click", function() {
+    changeGrid();
+});
+
+function changeGrid() {
+    let child = container.lastElementChild;
+    while(child) {
+        container.removeChild(child);
+        child = container.lastElementChild;
+    }
+
+    gridSize = prompt("Choose the dimensions of the grid. Pick a number 1-100");
+    while(isNaN(gridSize) || gridSize < 1 || gridSize > 100) {
+        let tempGridSize = prompt("Please pick a valid number between 1-100");
+        gridSize = tempGridSize;
+    }
+
+    for (i = 1; i <= (gridSize ** 2); i++) {
+        const square = document.createElement("div");
+        const sideLength = `${700/gridSize}`;
+        square.style.cssText = `height: ${sideLength}px`;
+        square.style.cssText = `width: ${sideLength}px`;
+        square.id = "square";
+        square.addEventListener("mousemove", function() {
+            square.style.backgroundColor = "black";
+        })
+        container.appendChild(square);
+    }
+
+}
+
